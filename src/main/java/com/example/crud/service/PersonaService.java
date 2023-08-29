28193fe1552875e18db7605e1e77e43684545fb5
Facundo <wehrlifacundo9@gmail.com>
2023-08-29 12:51:21 -0300

    implement: override inteface methods & implement jpa repository methods

--- src/main/java/com/example/crud/service/PersonaService.java
+++ src/main/java/com/example/crud/service/PersonaService.java
@@ -1,9 +1,48 @@
 package com.example.crud.service;
 
-import com.example.crud.repository.PersonaRepository;
+import com.example.crud.model.Persona;
+import com.example.crud.repository.IPersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class PersonaService {
+import java.util.List;
+ @Service
+public class PersonaService implements IPersonaService {
+    //inyección de dependencias
     @Autowired
-    private PersonaRepository personaRepository;
+    private IPersonaRepository iPersonaRepository;
+
+    //sobreescribo la interfaz utilizando métodos de JpaRespository(findAll, save, delete, findById) mediante "DI",
+    @Override
+    public List<Persona> getPersonas() {
+        return iPersonaRepository.findAll();
+    }
+
+    @Override
+    public void savePersona(Persona perso) {
+        iPersonaRepository.save(perso);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        iPersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        return iPersonaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void editPersona(Long idOriginal, Long idNueva, String nuevoNombre, String nuevoApellido, int nuevaEdad) {
+        //uso "this" porque llamo al mismo método findPersona de esta clase
+        Persona perso = this.findPersona(idOriginal);
+        //proceso de modificación a nivel lógico
+        perso.setId(idNueva);
+        perso.setNombre(nuevoNombre);
+        perso.setApellido(nuevoApellido);
+        perso.setEdad(nuevaEdad);
+        //uso "this" porque llamo al mismo método savePersona de esta clase
+        this.savePersona(perso);
+    }
 }
